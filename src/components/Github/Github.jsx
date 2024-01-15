05a44ca703f97715547dd834ef62f44abd1fba30
Abhishek Kumar <abhishekkumar958830@gmail.com>
2024-01-15 12:59:35 +0530

    github components

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+
+function Github() {
+ const [data, setData] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/abhishek-kumar-91')
+        .then((response)=>response.json())
+        .then((data) => setData(data));
+    })
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    
+    </div>
+  )
+}
+
+export default Github
